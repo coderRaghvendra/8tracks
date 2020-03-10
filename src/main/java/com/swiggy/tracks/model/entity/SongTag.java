39927646e6d03c18eca589face408746d75a2ce3
Raghav Labs <labs.raghav@gmail.com>
2020-03-10 06:08:42 +0530

    entities created

---
+++ src/main/java/com/swiggy/tracks/model/entity/SongTag.java
@@ -0,0 +1,70 @@
+package com.swiggy.tracks.model.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Created by raghvendra.mishra on 10/03/20.
+ */
+public class SongTag {
+    private long id;
+    private UUID uid;
+    private long songId;
+    private long tagId;
+    private boolean status;
+    private LocalDateTime creationTime;
+
+    private SongTag(Builder b) {
+        this.id = b.id;
+        this.uid = b.uid;
+        this.songId = b.songId;
+        this.tagId = b.tagId;
+        this.status = b.status;
+        this.creationTime = b.creationTime;
+    }
+
+    public static class Builder {
+        private long id;
+        private UUID uid;
+        private long songId;
+        private long tagId;
+        private boolean status;
+        private LocalDateTime creationTime;
+
+        public Builder() {}
+
+        public Builder id(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder uid(UUID uid) {
+            this.uid = uid;
+            return this;
+        }
+
+        public Builder songId(long songId) {
+            this.songId = songId;
+            return this;
+        }
+
+        public Builder tagId(long tagId) {
+            this.tagId = tagId;
+            return this;
+        }
+
+        public Builder status(boolean status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder creationTime(LocalDateTime creationTime) {
+            this.creationTime = creationTime;
+            return this;
+        }
+
+        public SongTag build() {
+            return new SongTag(this);
+        }
+    }
+}
---
+++ src/main/java/com/swiggy/tracks/model/entity/Tag.java
@@ -0,0 +1,70 @@
+package com.swiggy.tracks.model.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Created by raghvendra.mishra on 10/03/20.
+ */
+public class Tag {
+    private long id;
+    private UUID uid;
+    private String name;
+    private long songCount;
+    private boolean status;
+    private LocalDateTime creationTime;
+
+    private Tag(Builder b) {
+        this.id = b.id;
+        this.uid = b.uid;
+        this.name = b.name;
+        this.songCount = b.songCount;
+        this.status = b.status;
+        this.creationTime = b.creationTime;
+    }
+
+    public static class Builder {
+        private long id;
+        private UUID uid;
+        private String name;
+        private long songCount;
+        private boolean status;
+        private LocalDateTime creationTime;
+
+        public Builder() {}
+
+        public Builder id(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder uid(UUID uid) {
+            this.uid = uid;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder songCount(long songCount) {
+            this.songCount = songCount;
+            return this;
+        }
+
+        public Builder status(boolean status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder creationTime(LocalDateTime creationTime) {
+            this.creationTime = creationTime;
+            return this;
+        }
+
+        public Tag build() {
+            return new Tag(this);
+        }
+    }
+}
